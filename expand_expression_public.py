4952427a86c88576eafa772b97844ac49554db26
retsyo <lepto.python@gmail.com>
2018-12-21 23:14:09 +0800

    Add files via upload

---
+++ expand_expression_public.py
@@ -0,0 +1,122 @@
+﻿from sympy import *
+
+__version__ = '2018.12.21'
+
+def latexMatrix2Array(strLatex):
+    res = strLatex.replace(r'\begin{matrix}', r'\begin{array}{r}')
+    res = res.replace(r'\end{matrix}', r'\end{array}')
+
+    return res
+
+class T_Rotate(object):
+    idx = 0
+
+    def __init__(self, theta, useRad=False, reset=False):
+
+        if reset == True:
+            T_Rotate.idx = 0
+
+        self.eq = '''Matrix([
+        [cos(theta{0}), -sin(theta{0}), 0],
+        [sin(theta{0}), cos(theta{0}), 0],
+        [0, 0, 1]
+    ])'''.format(T_Rotate.idx).replace('\n', '')
+
+        self.eq1_0 = sympify(self.eq)
+        self.eq1 = latex(self.eq1_0).replace(r'\frac', r'\dfrac')
+
+        # 传进来的是 字符串，转换为 表达式
+        if useRad: #  可以实现显示 xx rad、xx°吗？
+            self.theta = sympify(theta)
+        else:
+            self.theta = rad(sympify(theta))
+
+        print('theta = ', theta)
+        print('sympify(theta) = ', sympify(theta))
+        with evaluate(False):
+            if useRad:
+                self.eq2 = self.eq1_0.subs({'theta{0}'.format(T_Rotate.idx): sympify(theta)})
+            else:
+                self.eq2 = self.eq1_0.subs({'theta{0}'.format(T_Rotate.idx): sympify('rad(%s)' % theta)})
+
+        T_Rotate.idx += 1
+
+
+class T_LOG(object):
+    idx = 0
+
+    def __init__(self, x, reset=False):
+        if reset == True:
+            T_LOG.idx = 0
+
+        self.eq = 'log(x{0})'.format(T_LOG.idx)
+        self.eq1_0 = sympify(self.eq)
+
+        with evaluate(False):
+            self.eq2 = self.eq1_0.subs({'x{0}'.format(T_LOG.idx): sympify(x)})
+
+        T_LOG.idx += 1
+
+fOut = open('test.md', 'w')
+
+def latexExpression(expr):
+    with evaluate(False):
+        z1 = sympify(expr).subs({i.name: sympify(globals()[i.name].eq) for i in sympify(expr).free_symbols})
+
+        z2 = sympify(expr).subs({i.name: sympify(globals()[i.name].eq2) for i in sympify(expr).free_symbols})
+
+    z3 = simplify(z2)
+    z4 = z3.evalf()
+
+    res = (
+        '\n\n$$'
+        + r'\begin{align}'
+        + '\n%s &= ' % expr
+        + latexMatrix2Array(latex(z1))
+        + r'\\' +'\n' + '&='
+        + latexMatrix2Array(latex(z2))
+        + r'\\' +'\n' + '&='
+        + latexMatrix2Array(latex(z3))
+        + r'\\' +'\n' + '&='
+        + latexMatrix2Array(latex(z4))
+        + '\n'
+        + r'\end{align}$$'
+        + '\n\n'
+    )
+
+    return res.replace(r'\frac', r'\dfrac')
+
+
+T1 = T_Rotate('pi/6 + pi/2', useRad=True)
+fOut.write(latexExpression('T1'))
+fOut.write('\n\n')
+
+T1 = T_Rotate('pi/6', useRad=True)
+T2 = T_Rotate('-50')
+
+fOut.write(latexExpression('T1 * T2'))
+fOut.write('\n\n')
+
+T3 = T_Rotate('-20')
+fOut.write(latexExpression('T3'))
+fOut.write('\n\n')
+
+fOut.write(latexExpression('T1 + T2'))
+fOut.write('\n\n')
+
+L = T_LOG('sin(pi/3) * 2 + 1')
+fOut.write(latexExpression('L'))
+fOut.write('\n\n')
+
+L1 = T_LOG('2')
+L2 = T_LOG(' 3')
+L3 = T_LOG(' 5')
+fOut.write(latexExpression('L1 + L2 + L3'))
+fOut.write('\n\n')
+
+L4 = T_LOG('30')
+fOut.write(latexExpression('L4'))
+fOut.write('\n\n')
+
+fOut.close()
+
---
+++ readme.md
@@ -0,0 +1,37 @@
+# What?
+a function to express the 4 steps of math calculation in markdown format:
+step 1, list the formula
+step 2, replace the variants with specified values
+step 3, simplify the result of step 2
+step 4, evaluate the result to float
+
+# How?
+```python
+T1 = T_Rotate('pi/6 + pi/2', useRad=True)
+fOut.write(latexExpression('T1'))
+```
+will write a markdown file which can be rendered as
+$$
+\begin{align}
+T1 &= \left[\begin{array}{r}\cos{\left (\theta_{0} \right )} & - \sin{\left (\theta_{0} \right )} & 0\\\sin{\left (\theta_{0} \right )} & \cos{\left (\theta_{0} \right )} & 0\\0 & 0 & 1\end{array}\right]\\
+&=\left[\begin{array}{r}\cos{\left (\dfrac{\pi}{6} + \dfrac{\pi}{2} \right )} & - \sin{\left (\dfrac{\pi}{6} + \dfrac{\pi}{2} \right )} & 0\\\sin{\left (\dfrac{\pi}{6} + \dfrac{\pi}{2} \right )} & \cos{\left (\dfrac{\pi}{6} + \dfrac{\pi}{2} \right )} & 0\\0 & 0 & 1\end{array}\right]\\
+&=\left[\begin{array}{r}- \dfrac{1}{2} & - \dfrac{\sqrt{3}}{2} & 0\\\dfrac{\sqrt{3}}{2} & - \dfrac{1}{2} & 0\\0 & 0 & 1\end{array}\right]\\
+&=\left[\begin{array}{r}-0.5 & -0.866025403784439 & 0\\0.866025403784439 & -0.5 & 0\\0 & 0 & 1.0\end{array}\right]
+\end{align}
+$$
+
+# More?
+You can define your own functions in class. There are 2 in this release: `T_Rotate` and `T_LOG`
+
+**But I am also seeking for a more common method without supplying every functions by the user**
+
+# License
+You can use this function freely only if you mention that "Free expand_expression.py by Li Jun is used. Which can be download from https://github.com/retsyo/expand_expression"
+
+# Bug?
+1. please note the parameters are passed as a string not number/expression/sympy's expression
+2. if you find more bugs please report/fix it
+
+
+
+
